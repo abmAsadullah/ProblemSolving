7a7ca3b4a7e32ad19995df328cb3faaf4a04fc69
abmasadullah <abmasadullah@gmail.com>
2020-02-22 15:57:37 +0600

    Add files via upload

---
+++ UVA100_3nPlus1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    while(scanf("%d %d", &i, &j) != EOF){
+    //cin >>i >>j;
+    int a, b, n, r, f = 0;
+    a = i;
+    b = j;
+    if(a>b){
+        int tmp;
+        tmp =b;
+        b = a;
+        a = tmp;
+    }
+    for(int k=a;k<=b; k++)
+    {
+        n = k;
+        r = 1;
+        while(1)
+        {
+            if(n==1) break;
+            if(n%2==1) n= 3*n + 1;
+            else n = n/2;
+            r++;
+        }
+        if(r>=f) f=r;
+
+    }
+    cout <<i<<" "<<j <<" " <<f<<endl;
+    }
+    return 0;
+}
